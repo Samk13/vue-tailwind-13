26fa68a603928b9cb057e200f7046920d256d4fb
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2020-05-07 00:30:58 +0200

    can't show data in question box

--- components/QuizComps/HeaderFcc.vue
+++ components/QuizComps/HeaderFcc.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <h1>Header FCC</h1>
+  <div class="flex justify-center space-x-8 bg-teal-500 rounded-lg">
+    <h1 class="p-2 font-bold">Fancy Quiz App</h1>
+    <h1 class="p-2 font-bold">counter 4/10</h1>
   </div>
 </template>
--- components/QuizComps/QuestionBox.vue
+++ components/QuizComps/QuestionBox.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Qestion box</h1>
+    <template slot="question">{{ currentQuestion.category }}</template>
+    <hr class="my-4">
+    <p>List of answers</p>
+    <div class="space-x-4">
+      <button class="bg-green-400 px-4 py-1 rounded-lg">Submit</button>
+      <button class="bg-teal-400 px-4 py-1 rounded-lg">Next</button>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    currentQuestion: Object
+  }
+}
+</script>
+
