4809d7aba548ab8326aa58446a5295f35c8cbc29
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2020-05-06 11:08:08 +0200

    fix-2

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,8 @@
 <template functional>
-  <div class="card md:rounded shadow bg-white p-8 mb-8">
+  <div class="card rounded-lg shadow-lg bg-teal p-8 mb-8">
     <h1 class="text-3xl">{{props.title}}</h1>
-    <p class="my-4 leading-normal"><slot/></p>
+    <p class="my-4 leading-normal">
+      <slot/>
+    </p>
   </div>
 </template>
