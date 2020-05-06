81ddc9005419b70cc5ea6b3d08cff3f3e88f8e4f
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2020-05-06 10:44:48 +0200

    fix 1

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,8 +3,7 @@
     <div
       class="footer border-t border-grey-light py-4 container mx-auto max-w-md text-grey font-light text-sm tracking-wide flex justify-between"
     >
-      <span>2019(C) Kyli Rahn</span>
-      <a href="https://cosmiq.cc/kyli" class="no-underline text-grey hover:text-grey-dark">Cosmiq.cc</a>
+      <span>2020(C) Sam Arbid</span>
     </div>
   </div>
 </template>
