0f24635fb9646eb636d6b187855c8ca54743b8af
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2020-05-29 18:26:23 +0200

    setup transitions

---
+++ components/Transitions/FadeTransition.vue
@@ -0,0 +1,48 @@
+<template>
+  <component
+    :is="componentType"
+    :tag="tag"
+    v-bind="$attrs"
+    v-on="hooks"
+    enter-active-class="fadeIn"
+    move-class="fade-move"
+    leave-active-class="fadeOut"
+  >
+    <slot/>
+  </component>
+</template>
+<script>
+import { defineComponent } from "@vue/composition-api"
+import baseTransition from "./baseTransition.js"
+
+export default defineComponent({
+  mixins: [baseTransition]
+})
+</script>
+<style>
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+.fadeIn {
+  animation-name: fadeIn;
+}
+@keyframes fadeOut {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+.fadeOut {
+  animation-name: fadeOut;
+}
+.fade-move {
+  transition: transform 0.3s ease-out;
+}
+</style>
---
+++ components/Transitions/index.js
@@ -0,0 +1,5 @@
+import FadeTransition from "./FadeTransition.vue"
+import ScaleTransition from "./ScaleTransition.vue"
+import CollapseTransition from "./CollapseTransition.vue"
+
+export { FadeTransition, ScaleTransition, CollapseTransition }
