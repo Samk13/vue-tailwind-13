15e1a36b8231ca99111f95fb28691e3d455587aa
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2020-05-29 18:03:28 +0200

    remove the FCC

--- components/QuizComps/HeaderFcc.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex justify-center space-x-8 bg-teal-500 rounded-lg">
-    <h1 class="p-2 font-bold">Fancy Quiz App</h1>
-    <h1 class="p-2 font-bold">counter 4/10</h1>
-  </div>
-</template>
