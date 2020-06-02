c9c030c0a7a6903559da5d669d65de86203ad4f8
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2020-06-03 00:44:57 +0200

    change fonts

--- components/Header.vue
+++ components/Header.vue
@@ -3,8 +3,11 @@
     <div class="container mx-auto flex items-center">
       <router-link
         to="/"
-        class="font-bold text-2xl text-white cursor-pointer no-underline hover:text-blue-500"
-      >Sam Testing</router-link>
+        class="font-thin text-2xl text-teal-600 cursor-pointer no-underline hover:text-orange-400"
+      >
+        Sam
+        <span class="text-orange-400">Testing</span>
+      </router-link>
       <!--     <div class="search flex-1 ml-8 mr-8">
         <input
           class="w-full text-grey-dark font-light py-2 bg-grey-lighter px-4 border border-grey-light rounded"
@@ -12,9 +15,9 @@
           type="text"
         >
       </div>-->
-      <div class="ml-auto space-x-4">
-        <router-link to="/transitions" class="no-underline hover:text-blue-500">Transitions</router-link>
-        <router-link to="/ripple" class="no-underline hover:text-blue-500">Ripple</router-link>
+      <div class="ml-auto space-x-4 font-thin text-teal-600">
+        <router-link to="/transitions" class="no-underline hover:text-orange-500">Transitions</router-link>
+        <router-link to="/ripple" class="no-underline hover:text-orange-500">Ripple</router-link>
         <a
           href="https://www.linkedin.com/in/samarbid/"
           target="_blank"
