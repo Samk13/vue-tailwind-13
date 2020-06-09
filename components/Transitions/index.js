835161297185362f0d72cd7427ff85d8f60d3066
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2020-06-09 10:00:19 +0200

    add group transition

--- components/Transitions/index.js
+++ components/Transitions/index.js
@@ -1,5 +1,11 @@
 import FadeTransition from "./FadeTransition.vue"
 import ScaleTransition from "./ScaleTransition.vue"
 import CollapseTransition from "./CollapseTransition.vue"
+import ListTransitionCss from "./ListTransitionCss.vue"
 
-export { FadeTransition, ScaleTransition, CollapseTransition }
+export {
+  FadeTransition,
+  ScaleTransition,
+  CollapseTransition,
+  ListTransitionCss
+}
